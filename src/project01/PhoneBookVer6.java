74921b7e7eba8c6c3b14ea007cb5cd79f22201ea
dbxodud89 <dbxodud89@naver.com>
2021-10-29 09:30:48 +0900

    7단계중복체크...해야함

--- src/project01/PhoneBookVer6.java
+++ src/project01/PhoneBookVer6.java
@@ -3,65 +3,53 @@ package project01;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import project01.ver06.MenuSelectException;
 import project01.ver06.Menuitem;
+import project01.ver06.MenuSelectException;
 import project01.ver06.PhoneBookManager;
-import project01.ver06.Phoneinfo;
-import project01.ver06.SubMenuitem;
 
-public class PhoneBookVer6 implements Menuitem
-{
+public class PhoneBookVer6 implements Menuitem{
 	
-	public static void main(String[] args) throws MenuSelectException
-	{
-		Scanner scan  = new Scanner(System.in);
+	public static void main(String[] args) throws MenuSelectException {
+		//String name, String phoneNumber, String birthday
 		PhoneBookManager pho1 = new PhoneBookManager(100);
-				while(true)
-				{
-					pho1.menuShow();
-					try
-					{
-						int choice = scan.nextInt();
-						if(choice >= 1 && choice <= 5)
-						{
-							switch(choice)
-							{
-							case ADD:
-								pho1.dataInput();
-								break;
-							case SER:
-								pho1.dataSearch();
-								break;
-							case DEL:
-								pho1.dataDelete();
-								break;
-							case ALL:
-								pho1.dataAllShow();
-								break;
-							case EXI:
-								System.out.println("프로그램을 종료합니다.");
-								return;
-							}
-						}
-						else
-						{
-							MenuSelectException mse = new MenuSelectException();
-							throw mse;
-						}
+		
+		int choice = 0;
+		
+		while(true) {
+			pho1.menuShow();
+			try {
+				
+				Scanner scan = new Scanner(System.in);
+				choice = scan.nextInt();
+				
+				if(choice >= 1 && choice <= 5) {
+					
+					switch (choice) {
+					case ADD:
+						pho1.dataInput();
+						break;
+					case SER:
+						pho1.dataSearch();
+						break;
+					case DEL:
+						pho1.dataDelete();
+						break;
+					case ALL:
+						pho1.dataAllShow();
+						break;
+					case EXI:
+						System.out.println("프로그램을 종료합니다.");
 						return;
-					}
-					catch (InputMismatchException e) 
-					{
-						System.out.println("입력이 잘못됨");
-						e.printStackTrace();
-						System.exit(0);
-					}
-					catch (NullPointerException e) 
-					{
-						System.out.println("입력된것이 없음");
-						e.printStackTrace();
-					}
-				}
+					}					
+				}else {
+					MenuSelectException menuSelectErr = new MenuSelectException("숫자1~5만 입력하세요");
+					continue;
+				} 
+			}catch (InputMismatchException e) {				
+				System.out.println("숫자를 입력하세요 ");
+			}catch(NullPointerException e) {
+				System.out.println("입력된값이 없습니다.");
+			}
+		}
 	}
 }
-
--- src/project01/ver06/MenuSelectException.java
+++ src/project01/ver06/MenuSelectException.java
@@ -2,8 +2,9 @@ package project01.ver06;
 
 public class MenuSelectException extends Exception
 {
-	public MenuSelectException()
-	{
-		super("숫자는 1~5까지만 입력하세요");
-	}
+	 public MenuSelectException(String m)
+	 {
+		 System.out.println(m);
+	 }
+	 
 }
